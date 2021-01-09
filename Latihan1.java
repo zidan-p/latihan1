d4e6a2c1e4ece1bb9661a9efe008239bf025a032
Zidan Putra Rahman <68506833+zidan-p@users.noreply.github.com>
2021-01-09 08:45:44 +0700

    Add files via upload

---
+++ Latihan1.java
@@ -0,0 +1,14 @@
+
+package latihanPBO;
+
+
+public class Latihan1 {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello"); //menampilkan kalimat
+        System.out.println("Selamat Belajar di program Java"); //menmapilkan kalimat
+        
+    }
+    
+}
